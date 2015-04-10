e728ed39c195a5ccf92325e7d5101f132f328f96
wyq <751239183@qq.com>
2015-04-10 16:31:07 +0800

    add zhidahao

---
+++ topics/zhidahao.php
@@ -0,0 +1,126 @@
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="renderer" content="webkit">
+    <title>百度直达号</title>
+    <meta name="keywords" content="诺辉科技,微网站,微信营销,微网站开发,微网站代理,微网站制作,微信网站,微信3G网站,微信APP,企业微信,企业微网站,微信公众号,微信互动">
+    <meta name="description" content="诺辉科技是由杭州微巴信息技术有限公司研发的一款基于微信营销的产品，可以帮助企业快速通过微信获得生意，推广企业品牌。">
+    <link rel="stylesheet" type="text/css" href="http://www.weiba66.com/app/modules/pc/assets/css/base.css">
+    <link rel="stylesheet" type="text/css" href="http://www.weiba66.com/app/modules/pc/assets/css/common.css">
+    <script type="text/javascript" src="http://www.weiba66.com/app/modules/pc/assets/js/jquery.min.js"></script>
+</head>
+<body>
+<!-- <div class='test'>1</div> -->
+<?php include_once("../nav.php")?>
+<link rel='stylesheet' href='http://www.weiba66.com/zt/zhidahao/css/index.css'>
+<div class="main">
+    <div style="width:100%; height:506px; background:url(http://www.weiba66.com/zt/zhidahao/images/bannerbg.jpg) no-repeat center">
+        <div style="width:1001px; margin:0 auto; padding-top:100px; position:relative">
+            <img src="http://www.weiba66.com/zt/zhidahao/images/banner.png" /><a class="btn_lt" href="/apply.html?from_site=zhida.weiba666.com"></a><a class="jm_btn" href="/join.html"></a>
+        </div>
+    </div>
+    <div style="width:1020px; margin:0 auto; padding:60px 0;">
+        <div style="float:left; width:368px; height:280px; border:1px solid #ddd; padding:10px;">
+            <embed src="http://player.video.qiyi.com/36b881371fc058bf3d8d3e9cda64002c/0/0/w_19rso63rzh.swf-albumId=2527998209-tvId=2527998209-isPurchase=0-cnId=20" allowFullScreen="true" quality="high" width="368" height="280" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash"></embed>
+        </div>
+        <div style="float:right; width:590px;"><img src="http://www.weiba66.com/zt/zhidahao/images/zdh.jpg" /></div>
+        <div style="clear:both"></div>
+    </div>
+    <div class="mod-direct-main" style="width:100%; text-align:center; border-top:1px solid #ddd; border-bottom:1px solid #ddd">
+        <div class="direct-wp">
+            <h3 class="di-title">核心优势</h3>
+            <a href="/topics/zhidahao/ability.html" class="more" target="_blank">详情</a>
+            <div class="di-box">
+                <ul class="core-l clearfix">
+                    <li class="core-i spread"><a href="http://www.weiba66.com/topics/zhidahao/ability.html#createnew" target="_blank" class="core-img"></a>
+                        <div class="core-tip">广拉新</div>
+                        <div class="small-tip">精准对接亿万移动用户</div>
+                    </li>
+                    <li class="core-i transfer"><a href="http://www.weiba66.com/topics/zhidahao/ability.html#hightrans" target="_blank" class="core-img"></a>
+                        <div class="core-tip">高转化</div>
+                        <div class="small-tip">高效转化用户为客户，轻松赚钱</div>
+                    </li>
+                    <li class="core-i survival"><a href="http://www.weiba66.com/topics/zhidahao/ability.html#strong" target="_blank" class="core-img"></a>
+                        <div class="core-tip">强留存</div>
+                        <div class="small-tip">全方位的用户管理分析</div>
+                    </li>
+                    <li class="core-i easyopen"><a href="http://www.weiba66.com/topics/zhidahao/ability.html#easygo" target="_blank" class="core-img"></a>
+                        <div class="core-tip">易开通</div>
+                        <div class="small-tip">丰富的行业模板，一键开通</div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="mod-direct-main">
+        <div class="direct-wp ">
+            <h3 class="di-title">典型案例</h3>
+            <div class="case-info-bg">
+                <div class="case-info q-ui-follownav">
+                    <ul class="case-list">
+                        <li class="item"><i class="ilogo haidilao"></i>
+                            <p class="text">海底捞</p>
+                        </li>
+                        <li class="item current"><i class="ilogo liangzi"></i>
+                            <p class="text">良子健身</p>
+                        </li>
+                        <li class="item"><i class="ilogo emeishan"></i>
+                            <p class="text">峨眉山</p>
+                        </li>
+                    </ul>
+                    <div class="case-list-arrow slider-arrow" style="left:341.5px"></div>
+                </div>
+            </div>
+            <div class="case-all-info">
+                <div class="case-all">
+                    <div class="case-hover haidilao dis"><img class="case-img" src="http://www.weiba66.com/zt/zhidahao/images/bg_haidilao_b3693e1b.jpg">
+                        <div class="text">
+                            <p class="title">海底捞</p>
+                            <p class="info">已有二十年历史的国内知名餐饮品牌，将川味火锅带到了全国20多个城市，海底捞轻应用为百度移动搜索用户提供了预订餐位、外卖、菜品介绍等丰富的功能。通过直达号用户在搜索海底捞后即可直达海底捞轻应用官网，快速订座或点外卖，真正做到了即搜即用。在用户服务体验、企业内部管理等层面都走在餐饮行业内领先位置的海底捞，再一次在信息化层面为其他餐饮企业树立了标杆，为越来越多的移动互联网用户提供更优的用餐体验。</p>
+                            <img class="qr" src="http://www.weiba66.com/zt/zhidahao/images/tdc_haidilao_35bebcac.jpg"></div>
+                    </div>
+                    <div class="case-hover liangzi dis"><img class="case-img" src="http://www.weiba66.com/zt/zhidahao/images/bg_liangzi_714f07a9.jpg">
+                        <div class="text">
+                            <p class="title">良子健身</p>
+                            <p class="info">北京朱国凡良子健身技术发展有限公司创立于1997年2月，是以健身、保健、休闲、养生为主的大型连锁企业。总部设在北京，全国有400多家连锁店，员工万人。旗下拥有朱国凡良子、非凡足迹、姈泉、沐籣遇四大品牌。通过直达号，用户可以直达良子健身轻应用，为用户提供门店导航、优惠信息、服务预约等服务，用户也可以进服务行咨询、效果评价等。</p>
+                            <img class="qr" src="http://www.weiba66.com/zt/zhidahao/images/tdc_liangzi_27427799.jpg"></div>
+                    </div>
+                    <div class="case-hover emeishan"><img class="case-img" src="http://www.weiba66.com/zt/zhidahao/images/bg_emeishan_51f59126.jpg">
+                        <div class="text">
+                            <p class="title">峨眉山</p>
+                            <p class="info">峨眉山，世界文化与自然遗产，中国四大佛教名山之一，国家5A级旅游景区。峨眉山旅游轻应用针对游览的景区信息获取、购票服务、景区导游这三个环节，为游客提供完整而流畅的旅游服务。通过直达号，用户搜索@峨眉山后直接到达峨眉山官方轻应用，避开了线上来源繁杂的非官方旅游信息，直接对接优质可信赖的信息和票务服务，进一步提升用户体验。</p>
+                            <img class="qr" src="http://www.weiba66.com/zt/zhidahao/images/tdc_emeishan_4049dbc1.jpg"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div style="text-align:center; padding:20px 0"><a href="/join.html"><img src="http://www.weiba66.com/zt/zhidahao/images/jm_btn.png"></a></div>
+</div>
+<script>
+    $(function(){
+        $('.case-list .item').hover(function(){
+            var index=$(this).index();
+            if(index==0)
+            {
+                $('.slider-arrow').css('left','341.5px');
+            }
+            if(index==1)
+            {
+                $('.slider-arrow').css('left','498.5px');
+            }if(index==2)
+            {
+                $('.slider-arrow').css('left','655.5px');
+            }
+            $('.case-all .case-hover').addClass('dis');
+            $('.case-all .case-hover').eq(index).removeClass('dis');
+        })
+    })
+</script>
+<?php include_once("../footer.php") ?>
+<!--请将以下码嵌入到您网页源代码的最后面，通常是</body></html>之后,这样在LR服务器升级维护的时候也不会影响您的网页打开呈现速度。-->
+<!-- <script language="javascript" src="http://dbt.zoosnet.net/JS/LsJS.aspx?siteid=DBT53393103&float=1&lng=cn"></script> --></body>
+</html>
