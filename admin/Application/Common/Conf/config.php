e37fda06c33cd0be53c33305f6695642ed2e5f2f
LinuxWyq <751239183@qq.com>
2015-04-15 11:28:42 +0800

    qudiaotiaoshi linux

--- admin/Application/Common/Conf/config.php
+++ admin/Application/Common/Conf/config.php
@@ -1,7 +1,7 @@
 <?php
 return array(
 	//'配置项'=
-    'SHOW_PAGE_TRACE' =>true,//调试
+   
     'DB_TYPE'   => 'mysql', // 数据库类型
     'DB_HOST'   => '112.126.80.235', // 服务器地址
     'DB_NAME'   => 'nuohui', // 数据库名
