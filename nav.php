2e05e39cabfc0f12be36bb8ac151dd7a09e434d7
wyq <751239183@qq.com>
2015-04-17 15:30:55 +0800

    ss

--- nav.php
+++ nav.php
@@ -86,7 +86,7 @@
                             							</li>
                                                                                     <li ><a href="/topics.php">专题</a>
                                                          							</li>
-                                                                                    <li ><a id="guanyu" href="/about.php">关于</a>
+                                                                                    <li id="guanyu"><a  href="/about.php">关于</a>
                                                                                          <div class="sun_list">
                                     <ul>
                                                                                     <li><a href="/about.php">关于我们</a></li>
